fc04c3a7636b896822d1bcfd11d9569751b754b8
zyzyx03 <fauzuwan.nazri93@gmail.com>
2021-05-08 19:49:32 +0800

    Building a CRUD application

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require("express")
+const mongoose = require('mongoose');
+const { MONGO_PASSWORD, MONGO_USER, MONGO_IP, MONGO_PORT } = require("./config/config");
 
-const app = express()
+const postRouter = require("./routes/postRoutes");
+
+const app = express();
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+
+const connectWithRetry = () => {
+    mongoose
+    .connect(mongoURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    })
+    .then(() => console.log("successfully connected to DB"))
+    .catch((e) => {
+        console.log(e)
+        setTimeout(connectWithRetry, 5000)
+    });
+}
+
+connectWithRetry();
+
+app.use(express.json());
 
 app.get("/", (req,res) => {
     res.send("<h1>Hi There!!!</h1>")
 })
 
+//localhost:3000/posts
+app.use("/api/v1/posts", postRouter)
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
